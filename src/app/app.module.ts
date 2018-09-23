bbeeff3f78986bfa012160e5ca7429f3c79cc401
yeison.quinto <yeison.quinto@.udea.edu.co>
2018-09-23 11:39:58 -0500

    inicio 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { RouterModule, Routes} from '@angular/router';
+
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UsuariosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
